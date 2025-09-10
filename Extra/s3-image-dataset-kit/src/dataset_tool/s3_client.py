8cc5ccf7c969f868745bf2a00dfab0ed1689e5b4
Devvyn Murphy <me@devvyn.ca>
2025-09-10 16:18:37 -0600

    ✨ add 'Extras' folder
    ✨ add 'Extras' folder with related scripts and example images

---
+++ Extra/s3-image-dataset-kit/src/dataset_tool/s3_client.py
@@ -0,0 +1,15 @@
+
+from __future__ import annotations
+import boto3
+import os
+from .config import SETTINGS
+
+_session = None
+def session():
+    global _session
+    if _session is None:
+        _session = boto3.session.Session(region_name=SETTINGS.region)
+    return _session
+
+def s3_client():
+    return session().client("s3", endpoint_url=SETTINGS.endpoint_url or None)
